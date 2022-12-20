52b6da369d20137d0073fcc002edb4545b43851a
dimas-sgu <117515367+dimas-sgu@users.noreply.github.com>
2022-12-20 14:06:05 +0700

    Update test.py

--- test.py
+++ test.py
@@ -8,8 +8,7 @@ def login():
   db = pymysql.connect("localhost")
   cursor = db.cursor()
 
-  # Execute the vulnerable SQL query concatenating user-provided input.
-  cursor.execute("SELECT * FROM users WHERE username = '%s' AND password = '%s'" % (username, password))
+
 
   # If the query returns any matching record, consider the current user logged in.
   record = cursor.fetchone()
